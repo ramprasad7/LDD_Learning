dbea359c25d17eeef3b508cd0bdaa66510d84c35
ramprasad7 <bandiramprasad7@gmail.com>
2022-07-21 10:27:13 +0530

    inversion count updated

--- C_Codes/Inversion_count.c
+++ C_Codes/Inversion_count.c
@@ -2,12 +2,14 @@
 
 int main(int argc,char **argv){
     int i,j,n,count=0;
+    printf("Enter the number of elements in the array\n");
     scanf("%d",&n);
     int a[n];
+    printf("Enter the elements in the array\n");
     for(i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
-
+    
     for(i=0;i<n;i++){
         for(j=i+1;j<n;j++){
             if(a[i] > a[j]){
@@ -15,6 +17,6 @@ int main(int argc,char **argv){
             }
         }
     }
-    printf("%d\n",count);
+    printf("The inversion count of the array is %d\n",count);
     return 0;
 }
