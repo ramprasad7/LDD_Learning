7d29f0623c807b0f9b7b49356dd08a45b4cca340
ramprasad7 <103480684+ramprasad7@users.noreply.github.com>
2022-09-06 14:50:53 +0530

    Update char_drv.c

--- Assignment/chardrv/char_drv.c
+++ Assignment/chardrv/char_drv.c
@@ -3,8 +3,47 @@
 #include<linux/kernel.h>
 #include<linux/version.h>
 
-int device_open(struct inode *pinode, struct file *fp);
+int major;
 
+int device_open(struct inode *pinode, struct file *fp){
+	printk("Device has been opened\n");
+	return 0;
+}
+
+
+ssize_t device_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset){
+
+	if(copy_to_user(buffer,device_buffer,length)){
+		printk("writing failed\n");
+		return -1;
+	}
+	else{
+		printk("writing to user success <%s>\n",device_buffer);
+	}	
+	printk("Device requested for read for %ld number of bytes\n",length);
+	
+	return len;
+}
+
+ssize_t device_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset){
+
+	if(copy_from_user(device_buffer,buffer,length)){
+		printk("failed to write\n");
+		return -1;
+	}
+	else{
+		printk("copying from user success with <%s>\n",buffer);
+	}
+
+	printk("Device requested for write for %ld number of bytes\n",length);
+	return length;
+}
+
+
+int device_release(struct inode *pinode,struct file *fp){
+	printk("Device has been closed\n");
+	return 0;
+}
 
 struct file_operations fops = {
 	.open = device_open,
@@ -16,10 +55,17 @@ struct file_operations fops = {
 
 static int hello_init(void){
 	printk(KERN_ALERT "Hello World! from %s\n",__func__);
+	major = register_chrdev_region(0,1,"Example_Driver");
+	if(major < 0){
+		printk("Failed to register device\n");
+	}else{
+		printf("Major Number: %d\n",major);
+	}
 	return 0;
 }
 
 static void hello_exit(void){
+	unregister_chrdev_region(major,1);
 	printk(KERN_ALERT "Good Bye! %s\n",__func__);
 }
 
@@ -28,8 +74,7 @@ module_init(hello_init);
 module_exit(hello_exit);
 
 
-MODULE_AUTHOR("Jyothirmai");
-MODULE_DESCRIPTION("alias Gudur Bidda");
+MODULE_AUTHOR("Example User");
+MODULE_DESCRIPTION("Example Driver");
 MODULE_LICENSE("GPL");
 MODULE_VERSION("1.0");
-:wqi
